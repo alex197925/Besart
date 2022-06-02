1b6f11a44fcd6e4524bbf967fe7aaf7850bf0e08
Besart Elezi <besart.elezi@hotmail.com>
2022-06-02 10:01:42 +0200

    updated page information with JS
    I also added some images that are used on the page

---
+++ script.js
@@ -0,0 +1,33 @@
+//Changing HTML elements
+document.getElementsByTagName("h1")[1].innerHTML = "My favourite colour";
+document.getElementsByTagName("h2")[0].innerHTML = "As much as I love all colours, blue has to be my favourite one! Because, just like a wise man once said: I'm Blue Da ba dee da ba di";
+document.getElementsByTagName("h1")[2].innerHTML = "My spirit animal";
+document.getElementsByTagName("h2")[1].innerHTML = "The animal for me has got to be the bear. It's big, strong, savage, yet can sometimes look like a very big goofy dog. It's the best animal there is and I will fight anyone who disagrees.";
+document.getElementsByTagName("h1")[3].innerHTML = "My favourite sport";
+document.getElementsByTagName("h2")[2].innerHTML = "Boxing is the sport that I absolutely adore. Nothing gets my blood boiling like watching a fight of Mike Tyson. Some boxers like to get close and deal as much damage as possible, some are quick, rely on their footwork and wait for an opening to strike, while others put on pressure, wait for their enemies to make a mistake, and counter them into oblivion. Seeing who's style of boxing triumphs over the other one's is what about the sport.";
+document.getElementsByTagName("h1")[4].innerHTML = "My favourite fictional character";
+document.getElementsByTagName("h2")[3].innerHTML = "Speaking of boxing, my favourite character is Takamura Mamoru. He's a main character from the anime Hajime no Ippo. I love everything about his character, from his gross jokes to his iron determination, he's just the best character I've ever seen. Takamura Mamoru can do anything!";
+document.getElementsByTagName("h1")[5].innerHTML = "My favourite celebrity";
+document.getElementsByTagName("h2")[4].innerHTML = "Danny 'Big Daddy' Devito takes his well-earned spot as my favourite celebrity. He won over my heart with his amazing performance in It's Always Sunny in Philadelphia, and by being an amazing human being."
+document.getElementsByTagName("h1")[6].innerHTML = "Besart Elezi";
+document.getElementsByTagName("p")[0].innerHTML = "Bolbi";
+document.getElementsByTagName("p")[1].innerHTML = "Strogganofski";
+document.getElementsByTagName("p")[2].innerHTML = "Superfan";
+
+//Changing my favourite colours
+document.getElementById("red").style.backgroundColor = "blue";
+document.getElementById("pink").style.backgroundColor = "lightblue";
+document.getElementById("green").style.backgroundColor = "lightskyblue";
+document.getElementById("yellow").style.backgroundColor = "cyan";
+
+//Changing the images
+document.getElementsByTagName("img")[0].src = "image/bear.jpg"
+document.getElementsByTagName("img")[1].src = "image/boxing.gif"
+document.getElementsByTagName("img")[2].src = "image/takamura.gif"
+document.getElementsByTagName("img")[3].src = "image/dannydevito.jpg"
+
+
+
+
+
+
